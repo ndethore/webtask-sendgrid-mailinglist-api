2e4b14e2453805ccbfe0496c90e810a3588a16f4
Nicolas <nicolas.dethore@gmail.com>
2018-05-29 02:33:58 +0900

    Add parse query parameter to decide wether to send welcome email or not

--- app.js
+++ app.js
@@ -24,15 +24,15 @@ function addSendgridRecipient(client, email, firstName) {
 }
 
 
-function sendWelcomeEmail(client, email, firstName) {
+function sendWelcomeEmail(client, email, firstName, senderEmail, senderName, welcomeTemplateID) {
 	return new Promise((fulfill, reject) => {
 		const data = {
 			from: {
-				email: context.secrets.SENDGRID_WELCOME_SENDER_EMAIL,
-				name: context.secrets.SENDGRID_WELCOME_SENDER_NAME
+				email: senderEmail,
+				name: senderName
 			},
 			reply_to: {
-				email: context.secrets.SENDGRID_WELCOME_SENDER_EMAIL
+				email: senderEmail
 			},
 			personalizations: [{
 				to: [{
@@ -43,7 +43,7 @@ function sendWelcomeEmail(client, email, firstName) {
 					"<%name%>": firstName
 				}
 			}],
-			template_id: context.secrets.SENDGRID_WELCOME_TEMPLATE_ID
+			template_id: welcomeTemplateID
 		};
 
 		const request = {
@@ -65,15 +65,26 @@ function sendWelcomeEmail(client, email, firstName) {
 
 
 module.exports = (context, cb) => {
-	// console.log(JSON.stringify(context));
-	let email = context.body.email;
-	let firstName = context.body.first_name;
+	console.log(JSON.stringify(context));
+	const email = context.body.email;
+	const firstName = context.body.first_name;
+	const welcomeEmail = context.query.welcome_email;
+
 	client.setApiKey(context.secrets.SENDGRID_API_KEY);
 	addSendgridRecipient(client, email, firstName)
 	.then((response) => {
-		sendWelcomeEmail(client, email, firstName)
-		.then( response => cb(null, response))
-		.catch(cb);
+
+		if (welcomeEmail === "true" || welcomeEmail === "1") {
+			const senderEmail = context.secrets.SENDGRID_WELCOME_SENDER_EMAIL;
+			const senderName = context.secrets.SENDGRID_WELCOME_SENDER_NAME;
+			const welcomeTemplateID = context.secrets.SENDGRID_WELCOME_TEMPLATE_ID;
+
+			sendWelcomeEmail(client, email, firstName, senderEmail, senderName, welcomeTemplateID)
+			.then( response => cb(null, response))
+			.catch(cb);
+		} else {
+			cb(null, response);
+		}
 	 })
 	 .catch(cb);
 };
